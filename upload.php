ba4a610df184d27fe115d599e15bc300488f9ba9
Suparna Chowdhury <kamela@metrodesk.com.bd>
2017-11-24 19:16:48 +0600

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,75 +2,64 @@
 require_once('../../../wp-load.php');
 require 'vendor/autoload.php';
 
-$sendgrid = new SendGrid('sendgrid api');
+$sendgrid = new SendGrid('api code');
 
-$mail = new SendGrid\Email();// send email to admin
+$mail = new SendGrid\Email();// send email to example
 $mail2 = new SendGrid\Email();// send confirmation email to client
 
-// Step 4: Post form data for process
+// Step 5: Post form data for process
 // Sender details
 $name = "example.Com";// Sender name
 $fname = $_POST['fname'];// Sender Name
 $email = $_POST['email'];// Sender Email
 $phone = $_POST['phone'];// Sender Telephone
 $description = $_POST['description'];// Sender Message
+
+// Step 1: Get token id to creat folder
 $token = $_POST['token'];//get token to create Order Id
 
-$target_dir = "/wp-content/uploads/trial/"; // Absolute path for directory where file will save
-$storeFolder = $target_dir .$token.'/'; 
-$originalName = $_FILES['file']['name'];
-$safeName     = cleanNameFunction($originalName);
-$target_file = $storeFolder . $safeName ;
-$tempFile = $_FILES['file']['tmp_name']; 
+// declare file store destination absolute paths
+$target_dir = "wp-content/uploads/trial/"; // Absolute path for directory where file will save
 
-$upload_dir = wp_upload_dir();
-$url = $upload_dir['baseurl'].'/'.$token.'/'.$safeName ;  
-// Step 1: Creat a direcotry to save file
-
+// Step 2: Creat a folder with token as folder name where file will get saved
+$storeFolder = $target_dir .$token.'/'; // add newly created folder to target path
 if(!file_exists($storeFolder) && !is_dir($storeFolder)) {
     mkdir($storeFolder);
 }
-// Step 2: Change file name according to setting
+
+// Step 3: Change file name according to setting
 function cleanNameFunction($name){
     $name = preg_replace("/[^a-zA-Z0-9.]+/", "", $name);
     return $name;
 }
+$originalName = $_FILES['file']['name'];
+$safeName     = cleanNameFunction($originalName);
 
-// Step 3: move the temp file to Server to store the file in target direcotry
+// Declare target path for file
+$tempFile = $_FILES['file']['tmp_name'];
+$target_file = $storeFolder . $safeName ;
+
+// Step 4: move the temp file to Server to store the file in target direcotry
 if (move_uploaded_file($tempFile, $target_file)) {
 $status = 1;
 }
 
-// //$images_arr = array();
-//     foreach($_FILES['file']['tmp_name'] as $key=>$val){
-//         //upload and stored images
-//         $tempFile = $_FILES['file']['tmp_name'][$key];
-//         $targetFile =  $storeFolder. $_FILES['file']['name'][$key];
-//         move_uploaded_file($tempFile,$targetFile);
-        
-//         // $tempFile = $_FILES['file']['tmp_name']; 
+// Step 5: Get file links
+$upload_dir = wp_upload_dir();
+$url = $upload_dir['baseurl'].'/'.$token.'/'.$safeName ; 
 
-//         // $target_file = $storeFolder . $_FILES['file']['name'][$key] ;
-//         // move_uploaded_file($_FILES['file']['tmp_name'][$key],$target_file);
-
-//         // if(move_uploaded_file($_FILES['file']['tmp_name'][$key],$target_file)){
-//         // 	$status = 1;
-//         //     $images_arr[] = $target_file;
-//         // }
-//     }
-
-// Step 5: Creat email based on Step 4 posted data
+// Step 6: Creat email based on Step 5 posted data
 $msg = "Free Trial Id: $token \r\n\r\nDear Admin,\r\n\r\nA new free trial has been submitted on example.com.\r\n\r\nName: $fname\r\n\r\nEmail Address: $email\r\n\r\nTelephone: $phone \r\n\r\n\r\nBrief Instruction: $description\r\n\r\n";
 
 
-	$msg .= "You can download the files from this link: \r\n \n$url \r\n\n\nPlease review the instruction carefully and process the trial order accordingly.\r\n\r\nKind Regards, \r\n\r\nImageEditing.com \r\n\r\nSkype: metrodeskbd[24 hours] \r\n\r\nEmail:support@imageediting.com";
+	$msg .= "You can download the files from this link: \r\n \n$url \r\n\n\nPlease review the instruction carefully and process the trial order accordingly.\r\n\r\nKind Regards, \r\n\r\nexample.com \r\n\r\n";
 	$recipient ="example@example.com";
-	$subject = "$token - New Free Trial from example";
+	$subject = "$token";
 	// Confirmation reciever
 	$confirm_subject = "Thank you for trying example.Com";
-	$confirm_msg = "Dear $fname,\r\n\r\nThank you for trying example.Com Free Trial service. Your free trial Id: $token. \r\n\r\nWe will notify you when your order is ready.\r\n\r\nFor any support you can reply to this email or call us at 00880 1741589509.\r\n\r\nKind Regards, \r\n\r\nImageEditing \r\n\r\nVisit: www.imageediting.com\r\n\r\nEmail: support@imageediting.com";
+	$confirm_msg = "Dear $fname,\r\n\r\nThank you for trying example.Com Free Trial service. Your free trial Id: $token. \r\n\r\nWe will notify you when your order is ready.";
 
-// Step 6: send email
+// Step 7: send email
 	// mail($recipient, $subject, $from, $mailheader) or die("Error!");
 	$mail->
 	addTo( $recipient )->
@@ -90,7 +79,7 @@ $msg = "Free Trial Id: $token \r\n\r\nDear Admin,\r\n\r\nA new free trial has be
 		exit(header('Location: https://example.com/thank-you/'));
 	}
 	else{
-		echo "Order submission failed. ";
+		echo "Order submission failed.";
 	}
 
 ?>
